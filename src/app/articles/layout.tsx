fbe99189451095b6d9dc6676da607034387373a8
OlehHodlevskyj <vt211_goo@student.ztu.edu.ua>
2024-03-04 21:39:19 +0200

    using ui component(alert)

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -1,18 +1,29 @@
 "use client"
-
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import styles from './articles.module.css';
+import Alert from '@mui/material/Alert';
 
-const LayoutArticles = () => {
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface LayoutArticlesProps {
+  posts: Post[];
+}
+
+const LayoutArticles = ({ posts }: LayoutArticlesProps) => {
   const pathname = usePathname();
-
   return (
     <div className="container mx-auto py-4">
       <nav className="mb-4">
         <ul className="flex space-x-4">
           <li className={pathname === '/articles' ? 'font-bold text-blue-500' : ''}>
-            <Link href="/articles">Articles</Link>
+            <Link href="/articles" className={styles.header}>Articles</Link>
           </li>
           <li className={pathname === '/articles/favorite' ? 'font-bold text-blue-500' : ''}>
             <Link href="/articles/favorite">Articles Favorite</Link>
@@ -22,10 +33,28 @@ const LayoutArticles = () => {
           </li>
         </ul>
       </nav>
+
+
+      {posts && posts.length > 0 ? (
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <Alert variant="filled" severity="info">
+          No post available.
+        </Alert>
+      )}
+
     </div>
   );
 };
 
 export default LayoutArticles;
+
 
 
